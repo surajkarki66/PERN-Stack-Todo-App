d860f0b8efe4c4da54f1470faadd8e5471a791be
surajkarki66 <suraj.karki500@protonmail.com>
2021-08-22 20:45:26 +0545

    Created express server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => {
+  console.log("server has started on port 5000");
+});
